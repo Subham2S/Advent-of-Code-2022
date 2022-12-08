57c967f6aca47be1a3a334cc001176fb7977d4bd
Subham Shit <subham.besume@gmail.com>
2022-12-08 13:51:09 +0530

    Added 1st v2

--- 1/1.py
+++ 1/1.py
@@ -1,5 +1,5 @@
 import sys
 from collections import defaultdict
-infile = sys.argv[1] if len(sys.argv) > 1 else '7.in'
+infile = '1.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
