bb90f9f362dbe4e5d0cf740710bb5e39c03e67e8
Subham Shit <subham.besume@gmail.com>
2022-12-08 14:24:35 +0530

    Solved 1st

--- 1/1.py
+++ 1/1.py
@@ -2,4 +2,19 @@ import sys
 from collections import defaultdict
 infile = '1.txt'
 data = open(infile).read().strip()
-lines = [x for x in data.split('\n')]
+lines = [x.strip() for x in data.split('\n')]
+
+elfs = defaultdict(int)
+i = 1
+for line in lines:
+    if line == '':
+        i += 1
+    else:
+        elfs[i] += int(line)
+
+# Part - 1
+print(elfs[max(elfs, key=elfs.get)])
+
+# Part - 2
+print(sum(
+    dict(sorted(elfs.items(), key=lambda item: item[1], reverse=True)[:3]).values()))
