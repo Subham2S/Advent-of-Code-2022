c1ba83663fc9ee044f15a46a6d23cb5bb024bf40
Subham Shit <subham.besume@gmail.com>
2022-12-08 13:48:41 +0530

    Solved 7th

--- 7/7.py
+++ 7/7.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-infile = sys.argv[1] if len(sys.argv) > 1 else '7.in'
+infile = sys.argv[1] if len(sys.argv) > 1 else '7.txt'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
