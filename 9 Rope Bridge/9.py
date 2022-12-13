9fa02b977edad2a582d0cf7af11a726965562d7f
Subham Shit <subham.besume@gmail.com>
2022-12-14 01:30:16 +0530

    Attempt 1 on 9th

--- 9 Rope Bridge/9.py
+++ 9 Rope Bridge/9.py
@@ -1,5 +1,8 @@
+import pandas as pd
+#import sys
 import numpy as np
-infile = '91.txt'
+# np.set_printoptions(threshold=sys.maxsize)
+infile = '9.txt'
 data = open(infile).read().strip()
 lines = [[x.split()[0], int(x.split()[1])] for x in data.split('\n')]
 
@@ -8,27 +11,72 @@ hgrid = np.zeros([2*n+1, 2*n+1])
 tgrid = np.zeros([2*n+1, 2*n+1])
 
 # Initializing the starting Position :
-hgrid[n, n], tgrid[n, n] = 1, 1
-hi, hj = n, n
-ti, tj = n, n
+hi, hj, ti, tj = n, n, n, n
+hgrid[hi, hj], tgrid[ti, tj] = 1, 1
 
-print(hgrid.compress(np.all(hgrid == 0, axis=0), axis=1))
-print(tgrid)
-# def tail_loc():
+# print(hgrid.compress(np.all(hgrid == 0, axis=0), axis=1))
+# print(tgrid)
 
 
-# for line in lines:
-#     print(line)
-#     if line[0] == 'R':
-#         for step in range(1, line[1]+1):
-#             hgrid[hi, hj+1] = 1
-#             tgrid[ti, tj] = 1
-#             j += 1
-#     elif line[0] == 'L':
-#         hgrid[m, :line[1]+1] = 1
-#     elif line[0] == 'U':
-#         ...
-#     else:
-#         ...
-#     print(hgrid)
-#     print(tgrid)
+def tail_loc(h1, h2, t1, t2, s):
+    if s == 'R':
+        if (((h1 == t1) & (h2 == t2)) | ((h1 == t1) & (h2 == t2 - 1)) |
+            (((h1 == t1 + 1) | (h1 == t1 - 1)) & (h2 == t2)) |
+                (((h1 == t1 + 1) | (h1 == t1 - 1)) & (h2 == t2 - 1))):
+            h2 += 1
+        elif ((h1 == t1) & (h2 == t2 + 1)):
+            h2 += 1
+            t2 += 1
+        else:
+            t1, t2 = h1, h2
+            h2 += 1
+    elif s == 'L':
+        if (((h1 == t1) & (h2 == t2)) | ((h1 == t1) & (h2 == t2 + 1)) |
+            (((h1 == t1 + 1) | (h1 == t1 - 1)) & (h2 == t2)) |
+                (((h1 == t1 + 1) | (h1 == t1 - 1)) & (h2 == t2 - 1))):
+            h2 -= 1
+        elif ((h1 == t1) & (h2 == t2 - 1)):
+            h2 -= 1
+            t2 -= 1
+        else:
+            t1, t2 = h1, h2
+            h2 -= 1
+    elif s == 'U':
+        if (((h1 == t1) & (h2 == t2)) | ((h2 == t2) & (h1 == t1 + 1)) |
+            (((h2 == t2 + 1) | (h2 == t2 - 1)) & (h1 == t1)) |
+                (((h2 == t2 + 1) | (h2 == t2 - 1)) & (h1 == t1 + 1))):
+            h1 -= 1
+        elif ((h2 == t2) & (h1 == t1 - 1)):
+            h1 -= 1
+            t1 -= 1
+        else:
+            t1, t2 = h1, h2
+            h1 -= 1
+    else:
+        if (((h1 == t1) & (h2 == t2)) | ((h2 == t2) & (h1 == t1 - 1)) |
+            (((h2 == t2 + 1) | (h2 == t2 - 1)) & (h1 == t1)) |
+                (((h2 == t2 + 1) | (h2 == t2 - 1)) & (h1 == t1 - 1))):
+            h1 += 1
+        elif ((h2 == t2) & (h1 == t1 + 1)):
+            h1 += 1
+            t1 += 1
+        else:
+            t1, t2 = h1, h2
+            h1 += 1
+    #print(h1, h2, t1, t2)
+    return h1, h2, t1, t2
+
+
+for line in lines:
+    s_ = line[0]
+    # print(line)
+    i = 1
+    while i <= line[1]:
+        hi, hj, ti, tj = tail_loc(hi, hj, ti, tj, s_)
+        hgrid[hi, hj] = 1 if hgrid[hi, hj] == 0 else 1
+        tgrid[ti, tj] = 1 if tgrid[ti, tj] == 0 else 1
+        i += 1
+
+# pd.DataFrame(hgrid).to_csv('./h.csv')
+# pd.DataFrame(tgrid).to_csv('./t.csv')
+print(sum(sum(tgrid)))
