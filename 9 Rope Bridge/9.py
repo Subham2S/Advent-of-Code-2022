f55cc9eba17a6da2df6869f73eb333c098e46114
Subham Shit <subham.besume@gmail.com>
2022-12-15 02:40:44 +0530

    Brainstorming 9th - Part 2

--- 9 Rope Bridge/9.py
+++ 9 Rope Bridge/9.py
@@ -2,7 +2,7 @@ import pandas as pd
 #import sys
 import numpy as np
 # np.set_printoptions(threshold=sys.maxsize)
-infile = '9.txt'
+infile = '92.txt'
 data = open(infile).read().strip()
 lines = [[x.split()[0], int(x.split()[1])] for x in data.split('\n')]
 
@@ -10,8 +10,7 @@ n = sum([x[1] for x in lines])
 hgrid = np.zeros([2*n+1, 2*n+1])
 tgrid = np.zeros([2*n+1, 2*n+1])
 
-# PART 1
-
+# PART 1 [H T]
 # Initializing the starting Position :
 hi, hj, ti, tj = n, n, n, n
 hgrid[hi, hj], tgrid[ti, tj] = 1, 1
@@ -87,4 +86,105 @@ save_array(hgrid, 'h')
 save_array(tgrid, 't')
 print(np.sum(tgrid))
 
-# PART 2
+# PART 2 [H 1 2 3 4 5 6 7 8 9]
+diff_dict = {
+    (1, 1): ['D', 'R'],
+    (1, -1): ['D', 'L'],
+    (-1, 1): ['U', 'R'],
+    (-1, -1): ['U', 'L'],
+    (0, 1): ['R'],
+    (1, 0): ['D'],
+    (0, -1): ['L'],
+    (-1, 0): ['U'],
+    (0, 0): ['O']
+}
+
+hgrid = np.zeros([2 * n + 1, 2 * n + 1])
+grid9 = np.zeros([2 * n + 1, 2 * n + 1])
+
+hi, hj = n, n
+i1, j1 = n, n
+i2, j2 = n, n
+i3, j3 = n, n
+i4, j4 = n, n
+i5, j5 = n, n
+i6, j6 = n, n
+i7, j7 = n, n
+i8, j8 = n, n
+i9, j9 = n, n
+
+hgrid[hi, hj], grid9[i9, j9] = 1, 1
+
+
+def single_move(i, j, s):
+    if s == 'R':
+        j += 1
+    elif s == 'L':
+        j -= 1
+    elif s == 'U':
+        i -= 1
+    else:
+        i += 1
+    return i, j
+
+
+def chk_shift(i_o, j_o, i_n, j_n):
+    row_diff, col_diff = i_n - i_o, j_n - j_o
+    # print(row_diff, col_diff)
+    steps = abs(row_diff) + abs(col_diff)
+    shift_type = diff_dict[(row_diff, col_diff)]
+    return shift_type, steps
+
+
+def next_knot(i_o, j_o, i_n, j_n, i_next, j_next, s_):
+    # print(i_o, j_o, i_n, j_n, i_next, j_next, s_)
+    st, steps = chk_shift(i_o, j_o, i_n, j_n)
+
+    if steps == 1:
+        i_o, j_o, i_next, j_next = tail_loc(i_o, j_o, i_next, j_next, st[0])
+    elif steps == 2:
+        s__ = s_ if s_ in st else st[0]
+        i_o, j_o, i_next, j_next = tail_loc(i_o, j_o, i_next, j_next, s__)
+        i_o, j_o, i_next, j_next = tail_loc(i_o, j_o, i_next, j_next,
+                                            [x for x in st if x != s__][0])
+    else:
+        pass
+    return i_next, j_next
+
+
+for line in lines:
+    s_ = line[0]
+    # print(line)
+    i = 1
+    while i <= line[1]:
+        # Recording past values:
+        i1_, j1_ = i1, j1
+        i2_, j2_ = i2, j2
+        i3_, j3_ = i3, j3
+        i4_, j4_ = i4, j4
+        i5_, j5_ = i5, j5
+        i6_, j6_ = i6, j6
+        i7_, j7_ = i7, j7
+        i8_, j8_ = i8, j8
+
+        hi, hj, i1, j1 = tail_loc(hi, hj, i1, j1, s_)
+        i2, j2 = next_knot(i1_, j1_, i1, j1, i2, j2, s_)
+        i3, j3 = next_knot(i2_, j2_, i2, j2, i3, j3, s_)
+        i4, j4 = next_knot(i3_, j3_, i3, j3, i4, j4, s_)
+        i5, j5 = next_knot(i4_, j4_, i4, j4, i5, j5, s_)
+        i6, j6 = next_knot(i5_, j5_, i5, j5, i6, j6, s_)
+        i7, j7 = next_knot(i6_, j6_, i6, j6, i7, j7, s_)
+        i8, j8 = next_knot(i7_, j7_, i7, j7, i8, j8, s_)
+        i9, j9 = next_knot(i8_, j8_, i8, j8, i9, j9, s_)
+
+        # print(hi, hj, i1, j1, i2, j2, i3, j3, i4, j4,
+        #       i5, j5, i6, j6, i7, j7, i8, j8, i9, j9)
+        # print("\n")
+        hgrid[hi, hj] = 1
+        grid9[i9, i9] = 1
+        i += 1
+
+# Save into CSV files:
+save_array(hgrid, 'h')
+save_array(grid9, 'tail_9')
+print(f'Total Positions of 9: {np.sum(grid9)}')
